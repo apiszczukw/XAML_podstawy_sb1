ad6c74c6e1c30090f2aad1c8c45bf254d6863333
apiszczukw <apiszczukw@gmail.com>
2025-03-23 08:11:01 +0100

    Dodaj pliki projektów.

---
+++ XAML_podstawy_sb1/Klasy/Osoba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAML_podstawy_sb1.Klasy
+{
+    public class Osoba
+    {
+        public string Imie;
+
+        public string Nazwisko;
+
+        public override string ToString()
+        {
+            return $"Cześć {Imie} {Nazwisko}! 😊🥑";
+        }
+    }
+}
